63f4043a4ab44cc88dc0f5686b58cce472084844
alan <ala.sotoo@duocuc.cl>
2026-05-26 22:04:12 -0400

    Add files via upload

--- ecocamping.py
+++ ecocamping.py
@@ -15,8 +15,48 @@ while ejecutando:
         print("Opcion no valida, porfavor seleccione entre 1 y5")
         continue
     #Despliegue de opciones
-    if opcion = 1:
+    if opcion == 1:
         disponibles= capacidad_maxima - sitios_ocupados
         print(f"\n[INFO] Sitioslibres para recibir vehiculos: {disponibles}")
+    elif opcion == 2:
+        sitios_libres = capacidad_maxima - sitios_ocupados
+        if sitios_libres == 0:
+            print("lo sentimos noquedan espacios en el camping")
+        else:
+            try:
+                ingreso = int(input("¿Cuantos sitios o vehiculos van a ingresar?"))
+                if ingreso <= 0:
+                    print("Error: la cantidad de ingreso debe ser mayora 0")
+                elif ingreso > sitios_libres:
+                    print(f"solo puede ingresar un maximo de {sitios_libres} sitios")
+                else:
+                    sitios_ocupados += ingreso
+                    print(f"ingreso registrado, se han ocupado {ingreso} de sitios")
+            except ValueError:
+                print("Error:debe ingresar un numero valido")
+    elif opcion == 3:
+        print(f"\n-- Registrar salida (vehiculos o sitios ocupados: {sitios_ocupados})")
+        if sitios_ocupados == 0:
+            print(" No hay vehiculos registrados en el camping actualmente")
+        else:
+            try:
+                salida = int(input("¿cuantos vehiculos se retiran?"))
+                if salida <= 0:
+                   print("Error: la cantidad debe ser mayor a 0")
+                elif salida > sitios_ocupados:
+                   print(f"Error: no se puede retirar mas de {sitios_ocupados} vehiculos")
+                else:
+                    sitios_ocupados -=salida
+                    print(f"salida registrada,sehan liberado {salida} sitios")
+            except ValueError:
+                print("Error:debe ingresar un numero entero valido")               
+    elif opcion == 4:
+        porcentaje_ocupacion = (sitios_ocupados / capacidad_maxima) * 100
+        print(f"\n[ESTADO] Ocupacion actual: {sitios_ocupados}/{capacidad_maxima} sitios")
+        print(f"[ESTADO] El camping esta al {porcentaje_ocupacion:.1f}% de su capacidad")
+    elif opcion == 5:
+        print("Cerrando el sistema")
+        ejecutando = False
     else:
         print("Opcion fuera de rango")
+    
